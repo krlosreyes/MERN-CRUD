cf2fdd00920802431d716bc11f527eef2159604a
CharlieDev'S <31894472+krlosreyes@users.noreply.github.com>
2024-06-01 09:52:26 -0500

    DevWev

---
+++ client/src/pages/TaskFormPage.jsx
@@ -0,0 +1,31 @@
+import {useForm} from 'react-hook-form';
+
+function TaskFormPage() {
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = handleSubmit((data)){
+        console.log(data);
+    }
+  return (
+    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+      <form onSubmit= {onSubmit}>
+        <input type="text" 
+        placeholder="Title" 
+        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        { ...register('title')}
+        autofocus/>
+        <textarea rows="3" 
+        placeholder="Description"
+        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-mdmy-2"
+        { ...register(description)}></textarea>
+        <input type="text" placeholder="Type" />
+        <button>
+            Save task
+        </button>
+        
+      </form>
+    </div>
+  )
+}
+
+export default TaskFormPage
